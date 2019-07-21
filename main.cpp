4183fb701b089942577cd16167810e30934ed2ad
Volkan Celik <52398564+celikvolkan@users.noreply.github.com>
2019-07-21 22:10:13 +0300

    Add files via upload
    unique tel number implemented.

--- main.cpp
+++ main.cpp
@@ -4,26 +4,24 @@
 //#include <cstring>
 #include <cstdlib>
 #include <ctime>
+#include <sstream>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-#define CUSTOMER_LIST_SIZE	(100)
+#define CUSTOMER_LIST_SIZE	(20000)
 
 using namespace std;
 string FirstName = "FName.txt";
 string SurName = "SName.txt";
 string CustomerList = "CustomerList.txt";
 
-//bool check_customer_existence(string &rstFile, string st);
-
 int main(int argc, char** argv) {
 	
 	ifstream fFName, fSName;	
 	fstream fCustomer;
 	string fname, sname, customer, tempBuffer;
-	int i, j, randNo1, randNo2, fNameCounter, sNameCounter, regenerationCounter = 0, customerCounter = CUSTOMER_LIST_SIZE;
-	
-	fstream f1;
-	
+	string const telnobase = "053"; 
+	int telno[4],  i, j, randNo1, randNo2, fNameCounter, sNameCounter, regenerationCounter = 0, customerCounter = CUSTOMER_LIST_SIZE;
+	//bool regnoflag = false;
 	fNameCounter = sNameCounter = 0;
 		
 	fFName.open(FirstName.c_str());
@@ -33,8 +31,6 @@ int main(int argc, char** argv) {
 	}
 		
 	while(getline(fFName, fname)){
-		//cout << fname << endl;
-		//cout << "file1 pointer " << fFName.tellg() << endl;
 		++fNameCounter;
 	}	
 	cout << "First name amount: " << fNameCounter << endl << endl;
@@ -45,7 +41,6 @@ int main(int argc, char** argv) {
 		exit(EXIT_FAILURE);
 	}	
 	while(getline(fSName, sname)){
-		//cout << sname << endl;
 		++sNameCounter;
 	}	
 	cout << "Surname amount: " << sNameCounter << endl;	
@@ -60,7 +55,7 @@ int main(int argc, char** argv) {
 	cout << endl;
 	while(customerCounter--)
 	{
-		REGENERATE:
+		REGENERATE_NAME:
 		randNo1 = rand() % fNameCounter + 1;
 		fFName.clear();
 		fFName.seekg(0, ios_base::beg);
@@ -75,18 +70,50 @@ int main(int argc, char** argv) {
 			getline(fSName, sname);
 		}
 		
-		customer = fname + " " + sname + "\n";	
-		//if ( true == check_customer_existence(tempBuffer, customer) ){		
+		customer = fname + " " + sname;			
 		if( tempBuffer.find(customer) != string::npos ){
 			if(++regenerationCounter < 10){
-				cout << "\t" << regenerationCounter << ". regeneration for " << customer;			
-				goto REGENERATE;
+				//cout << "\t" << regenerationCounter << ". regeneration for " << customer << endl;			
+				goto REGENERATE_NAME;
 			}
 		}
 		regenerationCounter = 0;
+		
+		REGENERATE_TEL_NO:
+		stringstream *pss = new stringstream ;
+		/* Add tel no 053x xxx xx xx */
+		pss->fill('0');
+		telno[0] = rand() % 10;
+		*pss << telnobase << telno[0] << ' ';
+		
+		pss->width(3);
+		telno[1] = rand() % 1000;
+		*pss << telno[1] << ' ';
+		
+		pss->width(2);
+		telno[2] = rand() % 100;
+		*pss << telno[2] << ' ';
+		
+		pss->width(2);
+		telno[3] = rand() % 100;
+		*pss << telno[3];
+		
+		//telno control
+		if( tempBuffer.find(pss->str()) != string::npos ){
+			if(++regenerationCounter < 10){
+				cout << "\t" << regenerationCounter << ". regeneration for " << pss->str() << "****************" << endl;
+				//regnoflag = true;			
+				goto REGENERATE_TEL_NO;
+			}
+		}		
+		regenerationCounter = 0;
+		
+		customer += "\t" + pss->str();
+		delete pss;
+		
+		customer += "\n";
 		cout << CUSTOMER_LIST_SIZE - customerCounter << " " << customer;
-		tempBuffer += customer;
-		// müþterinin yanýna numarasýný da ekle.			
+		tempBuffer += customer;	
 	}
 	fCustomer << tempBuffer;
 	
@@ -94,18 +121,9 @@ int main(int argc, char** argv) {
 	fSName.close();
 	fCustomer.close();
 	
+	//if(true == regnoflag)
+	//	cout << "Regeneration for no worked!!\n";
+	
 	return 0;
 }
-/*
-bool check_customer_existence(string &rstFile, string st)
-{
-	string line;
-	
-	while(){		
-		getline(rstFile, line);
-		if(line == st)
-			return true;			
-	}
-	return false;	
-}
-*/
+
